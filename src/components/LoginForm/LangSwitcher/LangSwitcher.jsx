ed34c6cb34528097a7471fddba8d9116f824d89a
Anastasiia Medved <nastya0662793393@gmail.com>
2025-04-04 12:11:05 +0200

    longrid

---
+++ src/components/LoginForm/LangSwitcher/LangSwitcher.jsx
@@ -0,0 +1,26 @@
+import { useId } from "react";
+import CSS from "./LangSwitcher.module.css";
+
+const LangSwitcher = ({ value, onSelect }) => {
+  const selectId = useId();
+  //   const [lang, setLang] = useState("uk");
+
+  return (
+    <div>
+      <label htmlFor={selectId}>Choose language</label>
+      <select
+        id={selectId}
+        value={value}
+        // value={lang}
+        // onChange={(evt) => setLang(evt.target.value)}
+        onChange={(evt) => onSelect(evt.target.value)}
+      >
+        <option value="en">English</option>
+        <option value="uk">Ukrainian</option>
+        <option value="pl">Polish</option>
+      </select>
+    </div>
+  );
+};
+
+export default LangSwitcher;
